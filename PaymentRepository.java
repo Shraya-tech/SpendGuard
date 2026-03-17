fce6d38003ea29b0eebcc4a9d6f3a1e50fdeed8b
ShrayaR <shraya.rajkarnikar@gmail.com>
2026-03-16 23:11:29 -0400

    Delete PaymentRepository.java

--- PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spendguard.payments;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    List<Payment> findByUserId(Long userId);
-}
