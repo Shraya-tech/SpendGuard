398a66001407d4c6e963372f4649b15c5d457299
ShrayaR <shraya.rajkarnikar@gmail.com>
2026-03-16 23:10:27 -0400

    Delete ContractRepository.java

--- ContractRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spendguard.contracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ContractRepository extends JpaRepository<Contract, Long> {
-    List<Contract> findByUserId(Long userId);
-}
