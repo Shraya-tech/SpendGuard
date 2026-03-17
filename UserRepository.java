4b5c707644e41ccba24c9d74294a48b75e3f3a83
ShrayaR <shraya.rajkarnikar@gmail.com>
2026-03-16 23:11:57 -0400

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spendguard.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
